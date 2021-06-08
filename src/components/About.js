ec177ee0a941f9be58210d049a4d13921aa1ff92
Adrienne <adriennepaquin@gmail.com>
2021-06-07 22:26:06 -0400

    commit tree 46b9880ca0ee30e53102709165daeb1d132ece6d

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,37 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {/* <p>{props.bio}</p> */}
+      {/* <p>{{props.bio} ? {props.bio} : ""}</p> */}
+      {/* <p>{getBio(props.bio)}</p> */}
+      {props.bio || props.bio === " " ? <p>{props.bio}</p> : null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={props.github} linkedin={props.linkedin} />
     </div>
   );
 }
+
+// function displayBio(bio){
+//   return {bio}
+// }
+
+// function getBio(bio){
+//   if (typeof bio != "undefined" && bio !== ""){
+//     return displayBio(bio)
+//   } 
+// }
+// function getBio(props){
+//   const bioExists = props.bio
+//   if (bioExists) {
+//     return <p>{props.bio}</p>
+//   } else {
+//     return <p style={{display:"none"}}></p>
+//   }
+// }
+
 
 export default About;
